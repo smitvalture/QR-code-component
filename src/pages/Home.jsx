4fdc1f07952722cc56fc1d23838e3e43dc04a03a
smit_valture <valture16@gmail.com>
2023-06-19 00:43:47 +0530

    done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import imageQR from '../assets/images/image-qr-code.png'
+
+const Home = () => {
+  return (
+    <section className='w-screen h-screen flex justify-center items-center bg-[#d6e0ed]'>
+      <div className='w-full mx-4 md:w-80 p-4 pb-10 flex flex-col gap-6 justify-center items-center bg-white shadow-xl rounded-xl'>
+        <img className='rounded-lg' src={imageQR} alt="image qr code" />
+        <h1 className='font-bold text-center text-[22px]'>Improve your front-end skills by building projects</h1>
+        <p className='text-gray-500 text-center font-medium'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+    </section>
+  )
+}
+
+export default Home
